238937ea73d4cb516e3dfe44bada67f664d52d3c
Apoorva Mandikal <apoorvagirish76@gmail.com>
2025-04-14 02:34:50 +0530

    fix: CORS error fix in py file

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000","https://d38bqbwa18mjva.cloudfront.net",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
